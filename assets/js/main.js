d07270b54f6af97059c13161eded6210ecf9e6e7
hoangminhduc1994 <151418180+hoangminhduc1994@users.noreply.github.com>
2024-07-03 22:22:23 +0700

    update-17

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let htmlContent = '';
         data.forEach(item => {
             htmlContent += `
-                <div class="col-12 col-sm-6 col-md-3">
+                <div class="col-12 col-sm-6 col-md-4">
                 <div class="post-recipe">
                 <img src="${item.thumbnail}" alt="recent-recipes-exps">
                 <p>${item.name}</p>
@@ -36,4 +36,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Load initial data from the first API
     fetchData('https://vm95y5-3000.csb.app/recipes');
+});
+
+
+
+// listing products
+document.addEventListener("DOMContentLoaded", () => {
+    const rowJs = document.querySelector(".row-js");
+  const apiUrl = "https://vm95y5-3000.csb.app/products";
+  const fetchData = async (url) => {
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        renderData(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+fetchData(apiUrl);
+const renderData = (data) => {
+    let htmlContent = '';
+    data.forEach(value => {
+        htmlContent += `
+            <div class="col-12 col-sm-6 col-md-4">
+            <div class="items">
+              <img src="${value.image}" alt="product">
+              <p>${value.price}</p>
+              <p>${value.name}</p>
+              <div class="more-detail">
+              <a href="#">More details</a>
+              </div>
+            </div>
+          </div>
+        `;
+    });
+rowJs.innerHTML = htmlContent;
+};
 });
