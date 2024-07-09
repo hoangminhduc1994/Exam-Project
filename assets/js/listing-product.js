46afba43a4f8312b6c7dd4c75c8c07db23f68c0b
hoangminhduc1994 <151418180+hoangminhduc1994@users.noreply.github.com>
2024-07-09 09:01:25 +0700

    update-40

--- assets/js/listing-product.js
+++ assets/js/listing-product.js
@@ -1,32 +1,70 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rowJs = document.querySelector(".row-js");
   const apiUrl = "https://vm95y5-3000.csb.app/products";
+  let fetchedData = [];
+
   const fetchData = async (url) => {
     try {
       const response = await axios.get(url);
-      const data = response.data;
-      renderData(data);
+      fetchedData = response.data;
+      renderData(fetchedData);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-  fetchData(apiUrl);
+
   const renderData = (data) => {
     let htmlContent = "";
     data.forEach((value) => {
       htmlContent += `
-            <div class="col-12 col-sm-6 col-md-4">
-            <div class="items" id="product-${value.id}">
-              <img src="${value.image}" alt="product">
-              <p>${value.price}</p>
-              <p>${value.name}</p>
-              <div class="more-detail">
+        <div class="col-12 col-sm-6 col-md-4">
+          <div class="items" id="product-${value.id}">
+            <img src="${value.image}" alt="product">
+            <p>${value.price}</p>
+            <p>${value.name}</p>
+            <div class="more-detail">
               <a href="product-detail.html?id=${value.id}">More details</a>
-              </div>
             </div>
           </div>
-        `;
+        </div>
+      `;
     });
     rowJs.innerHTML = htmlContent;
   };
+
+  // Lọc sản phẩm theo từ khóa
+  const filterBySearchTerm = (data, searchTerm) => {
+    return data.filter((item) => {
+      const oldTitle = item.name.toLowerCase();
+      return oldTitle.includes(searchTerm);
+    });
+  };
+
+  const filterProducts = () => {
+    let textSearch = document.querySelector("#search").value;
+    let searchTerm = textSearch.toLowerCase().trim();
+    let filteredData = filterBySearchTerm(fetchedData, searchTerm);
+    renderData(filteredData);
+  };
+
+  // Add event listener for the search button
+  document.querySelector("#searchButton").addEventListener("click", (event) => {
+    event.preventDefault();
+    filterProducts();
+  });
+
+  let clearTime;
+  // Lắng nghe sự kiện nhập liệu trên ô tìm kiếm
+  let inputSearch = document.querySelector("#search");
+  inputSearch.addEventListener("input", () => {
+    clearTimeout(clearTime);
+
+    // Neu sau 1s khong nhap nua thi moi goi vao filterProducts();
+    clearTime = setTimeout(() => {
+      filterProducts();
+    }, 1000); // 1000ms -> 1s
+  });
+
+  fetchData(apiUrl);
 });
+
